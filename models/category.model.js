b2683cd8e93cb7845b2376166639c1bbf1f6b89b
imtiyaz-Aayaninfotech <imtiyaz.hussain@aayaninfotech.com>
2025-07-09 15:31:46 +0530

    add category

--- models/category.model.js
+++ models/category.model.js
@@ -5,7 +5,6 @@ const categorySchema = new mongoose.Schema({
     type: String,
     required: true,
     trim: true,
-    unique: true,
     enum: ['Individual Therapy', 'Couples Counseling', 'Family Therapy', 'Group Therapy', 'Other']
   },
   type: {
